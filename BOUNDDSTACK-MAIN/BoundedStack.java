b9703fed3827269cd2a4c47dcd4d2b4019f90fad
nattavech-lab <nattavech.k@ku.th>
2026-07-26 10:44:21 +0700

    Add files via upload

---
+++ BOUNDDSTACK-MAIN/BoundedStack.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+
+
+public class BoundedStack {
+
+    // private final List<String> elements;
+//     private final int capacity;
+
+
+//AF(element,capicity)=
+//NI
+//-
+//-
+}
